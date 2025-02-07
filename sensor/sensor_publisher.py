09af25d7376558b41cb0a496c25b4e0d096c4b85
kmashao <karabomashao1@gmail.com>
2025-02-08 01:36:14 +0200

    Refifing the termperature sensor script

--- sensor/sensor_publisher.py
+++ sensor/sensor_publisher.py
@@ -5,17 +5,18 @@ import random
 
 BROKER = "localhost"
 PORT = 1883
-TOPIC_TEMPLATE = "sensors/temperature/{}"
-SENSORS = ["sensor1", "sensor2", "sensor3", "sensor4", "sensor5"]
+KEEP_ALIVE = 60
+TOPIC = "sensors/temperature/{}"
+SENSORS = ["sensor1", "sensor2", "sensor3", "sensor4", "sensor5"] #Five sensors for 5 rooms in the building
 
-client = mqtt.Client()
-client.connect(BROKER, PORT, 60)
+client = mqtt.Client()  #Creating client object
+client.connect(BROKER, PORT, KEEP_ALIVE) #Connecting to the broker and checking connection every 60 seconds
 
 try:
     while True:
         for sensors in SENSORS:
             temperature = round(random.uniform(15, 35), 2)
-            topic = TOPIC_TEMPLATE.format(sensors)
+            topic = TOPIC.format(sensors)
             message = f"{temperature}\u00b0C"
 
             client.publish(topic, message, qos=1)
@@ -24,7 +25,7 @@ try:
         time.sleep(2)
 
 except KeyboardInterrupt:
-    print("Stopping sensor simulation")
+    print("\nStopping sensor simulation")
     client.disconnect()
 
     
